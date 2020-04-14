2222465e7f9875c9fd2c6329453b5725571ada0f
qiandayu17 <qian.d@husky.neu.edu>
2020-04-14 18:31:33 -0400

    express server with simple ejs

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-import http from 'http';
+import express from 'express';
+import config from './config';
+import apiRouter from './api'
 
-const server = http.createServer((req, res) => {
-    res.write('Hello HTTP!\n');
-    setTimeout(() => {
-        res.write('I can stream!\n');
-        res.end();
-    },3000);
+const server = express();
+
+server.set('view engine', 'ejs')
+
+server.get('/', (req, res) => {
+    res.render('index', {
+        content: 'Hello Express and <em>EJS</em>!'
+    });
 });
 
-server.listen(8080);
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, () => {
+    console.info('Express listening on port ', config.port);
+})
